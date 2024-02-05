c08d11376102b93256d165b39dba394e1cb93915
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-02-05 13:48:04 +0000

    add json library moshi
    to exercise 1

--- another_training_module/exercise_1/src/main.kt
+++ another_training_module/exercise_1/src/main.kt
@@ -1,4 +1,14 @@
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+@OptIn(ExperimentalStdlibApi::class) // moshi adapters are experimental
 fun main() {
 	println("Exercise 1")
-	println(Person("Hello World"))
+	val person = Person("Hello World")
+	val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+	val jsonAdapter: JsonAdapter<Person> = moshi.adapter<Person>()
+	val json: String = jsonAdapter.toJson(person)
+	println(json)
 }
