721034e9f18e72126f9073fab13015eea2ec1bf9
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-02-06 19:43:54 +0000

    mention auto-download
    set to false to remove build warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@
 //plugins {
 //    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 //}
-
+// org.gradle.java.installations.auto-download=false is set in gradle.properties
 rootProject.name = "sample"
 includeBuild(".build-conventions") // composite build
 include("exercises:some_training_module:exercise_1", "exercises:some_training_module:exercise_2", "exercises:some_training_module:exercise_3", "exercises:some_training_module:exercise_4") // usual sub projects
