c856a060c46460b4a320806bb16b6c5f6a5f56a9
griff і⊙ <346896+griffio@users.noreply.github.com>
2024-02-06 15:51:22 +0000

    example of plugin for dependencies

--- exercises/another_training_module/exercise_1/build.gradle.kts
+++ exercises/another_training_module/exercise_1/build.gradle.kts
@@ -1,8 +1,4 @@
 plugins {
     id("griffio.exercise-conventions")
-}
-
-dependencies {
-    implementation("com.squareup.moshi:moshi:1.15.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+    id("griffio.exercise-moshi-libs")
 }
